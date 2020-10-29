b8f2625b2b466366a2f47d6f597bfc721c55284e
theblopa <pablo.garcia.lacalle1@gmail.com>
2020-10-29 23:10:20 +0100

    actualizado de pantallas

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.uix.button import ButtonBehavior
+from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.checkbox import CheckBox
 import kivy.utils
 #from MyFirebase import MyFirebase
 import requests
@@ -24,6 +26,18 @@ class Login(Screen):
 class ImageButton(ButtonBehavior, Image):
     pass
 
+class WrapButton(Button):
+    pass
+
+class ListRecipes(Screen):
+    pass
+
+class Recipes(Screen):
+    pass
+
+class Ingredients(Screen):
+    pass
+
 GUI = Builder.load_file("kv/main.kv", encoding="utf-8")
 
 class MainApp(App):
@@ -33,7 +47,11 @@ class MainApp(App):
         return GUI
 
     def on_start(self):
-        self.change_screen("home_screen")
+        result = requests.get("https://recetas-acfc9.firebaseio.com/1.json")
+        self.data = json.loads(result.content.decode())
+        print(self.data)
+        # self.change_screen("home_screen")
+        self.change_screen("list_recipes")
         recipe_text=self.root.ids['home_screen'].ids['receta_id']
         recipe_text.text="La lechuga se echa por la alcantarilla para despues aaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa servirla con perejil potado, salteado en una sarten con media mierdaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
@@ -43,6 +61,28 @@ class MainApp(App):
     
     def ping(self, n, value):
         print("funciona")
+    
+
+    def listado(self, option):
+        self.change_screen('recipes')
+        scroll = self.root.ids['recipes'].ids['l_recipe']
+        data = self.data[option]
+        for key in data:
+            btn=Button(text=str(key))
+            btn.bind(on_release=lambda a:self.receta(data, key))
+            scroll.add_widget(btn)
+
+    def receta(self, data, key):
+        print(key)
+        self.change_screen('ingredients')
+        scroll = self.root.ids['ingredients'].ids['l_recipe']
+        # print(data['ingredientes'])
+        # dat=data['ingredientes'].split(", ")
+        # for i in dat:
+        #     scroll.add_widget(Label(text=str(i)))
+        #     scroll.add_widget(CheckBox())
+
+
 
 if __name__ == "__main__":
     MainApp().run()
