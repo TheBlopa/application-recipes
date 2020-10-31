b9826e9b7568cdaa3763220809660d50c4067eab
theblopa <pablo.garcia.lacalle1@gmail.com>
2020-10-31 13:50:46 +0100

    cambio de diseno

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import kivy.utils
 #from MyFirebase import MyFirebase
 import requests
 import json
-import webbrowser
+# import webbrowser
 """from plyer import notification
 from plyer.compat import PY2
 from os.path import join, dirname, realpath"""
@@ -45,6 +45,12 @@ class Cooking(Screen):
 class MyLabel(Label):
     pass
 
+class MyCheckBox(CheckBox):
+    pass
+
+class Test(Screen):
+    pass
+
 GUI = Builder.load_file("kv/main.kv", encoding="utf-8")
 
 class MainApp(App):
@@ -54,55 +60,52 @@ class MainApp(App):
         return GUI
 
     def on_start(self):
+        # self.change_screen("test")
         result = requests.get("https://recetas-acfc9.firebaseio.com/1.json")
         self.data = json.loads(result.content.decode())
-        print(self.data)
-        # self.change_screen("home_screen")
-        self.change_screen("list_recipes",'')
+        self.change_screen("list_recipes")
         recipe_text=self.root.ids['home_screen'].ids['receta_id']
         recipe_text.text="La lechuga se echa por la alcantarilla para despues aaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa servirla con perejil potado, salteado en una sarten con media mierdaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
-    def change_screen(self, screen_name, prev):
+    def change_screen(self, screen_name):
         screen_manager = self.root.ids['screen_manager']
         screen_manager.current = screen_name
-        # hay que solucionar la repeticion de los widgets cunado se vuelve a cambiar
-        # de screen, probar con contador a la hora de añadir los elementos
-        # screen_manager.previous = prev
-        # if prev:
-        #     previ = self.root.ids[prev]
-        #     previ.clear_widgets()
-        #     print(screen_manager.previous)
     
     def ping(self, n, value):
         print("funciona")
     
 
     def listado(self, option):
-        self.change_screen('recipes','list_recipes')
+        self.change_screen('recipes')
         scroll = self.root.ids['recipes'].ids['l_recipe']
         head = self.root.ids['recipes'].ids['recipes_image']
         data = self.data[option]
-        head.add_widget(Image(source="icons/variedad.png"))
+        head.clear_widgets()
+        scroll.clear_widgets()
+        head.add_widget(Image(source="icons/recipe.png"))
         for key in data:
-            btn=Button(text=str(key), on_release=partial(self.receta, data=data[key]))
+            btn=Button(text=str(key), 
+                on_release=partial(self.receta, data=data[key]))
             scroll.add_widget(btn)
 
     def receta(self, caller, data):
-        self.change_screen('ingredients','recipes')
+        self.change_screen('ingredients')
         scroll = self.root.ids['ingredients'].ids['l_recipe']
+        scroll.clear_widgets()
         dat=data['ingredientes'].split(", ")
         for i in dat:
-            etiq=MyLabel(text=str(i))
+            etiq=MyLabel(text=str(i),color= (0,0,0,1))
             scroll.add_widget(etiq)
-            scroll.add_widget(CheckBox(size_hint_x=None, width=100, color=(1,1,0,1)))
-        button=ImageButton(source="icons/next.png", on_release=partial(self.cook, data=data['preparacion']))
-        # buttons=self.root.ids['ingredients'].ids['next']
-        # buttons.on_release=self.cook(data['preparacion'])
+            scroll.add_widget(MyCheckBox(size_hint_x=None, width=100))
+        button_next=ImageButton(source="icons/next.png", on_release=partial(self.cook, data=data['preparacion']))
+        button_back=ImageButton(source="icons/back.png", on_release=lambda x: self.change_screen('recipes'))
         buttons = self.root.ids['ingredients'].ids['buttons_grid']
-        buttons.add_widget(button)
+        buttons.clear_widgets()
+        buttons.add_widget(button_back)
+        buttons.add_widget(button_next)
     
     def cook(self, caller, data):
-        self.change_screen('cooking','ingredients')
+        self.change_screen('cooking')
         scroll = self.root.ids['cooking'].ids['receta_id']
         scroll.text=data
 
