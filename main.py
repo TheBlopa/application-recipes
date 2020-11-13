a23b743970c25105f1b43ebe63411ea1a975cfb0
unknown <pablo.garcia.lacalle1@gmail.com>
2020-11-13 12:56:36 +0100

    Finish all coments

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from manageDatabase import manageDatabase
 from applicationMethods import ApplicationMethods
-import requests
-import json
-"""from plyer import notification
-from plyer.compat import PY2
-from os.path import join, dirname, realpath"""
 
-# GUI = Builder.load_file("kv/main.kv", encoding="utf-8")
 
 class MainApp(MDApp):
+    """Main app to call all the functionality
+    Parameters
+    ----------
+    MDApp: root app from kivyMD"""
     # def __init__(self, **kwargs):
     #     super(MainApp, self).__init__(**kwargs)
     #     Window.bind(on_keyboard=self.on_key)
         
     def build(self):
+        """Define the database to use and the class with all the methods
+        Returns
+        -------
+            Build the interfaces"""
         self.database = manageDatabase()
         self.application = ApplicationMethods(self.database)
-        # Clock.schedule_interval(self.breath, 1)
         return Builder.load_file("kv/main.kv")
 
     def on_start(self):
-        # self.change_screen("login")
+        """Get all the information of the database, set the icons of all screens and change to the main screen"""
         self.data = self.database.get()
         self.application.icons(self)
         self.application.change_screen(self,"list_recipes")
@@ -33,14 +31,15 @@ class MainApp(MDApp):
 
 
     # def on_key(self, window, key, *args):
+    #     """Function of back button on the device
+    #     Parameters
+    #     ----------
+    #     window: object taht represent the keyboard of a device
+    #     key: int"""
     #     if key == 27:  # the esc key
     #         self.change_screen('home_screen')
 
-    # def breath(self, dtx):
-    #     anim = Animation(size_hint=(.15,.05), t= 'in_quad', duration= .5) + Animation(size=(.2,.1), t= 'in_quad', duration= .5)
-    #     tgt=self.root.ids['recipes'].ids['add']
-    #     anim.start(tgt)
-
 
 if __name__ == "__main__":
+    """Run the App"""
     MainApp().run()
--- manageDatabase.py
+++ manageDatabase.py
@@ -1,35 +1,54 @@
 import json
 import requests
 
-# Class for create and use the database that will be using in the appliaction
-
 class manageDatabase():
+    """Class to create and use the database that will be using in the appliaction"""
+    # url for your database, for example firebase
     url = 'https://recetas-acfc9.firebaseio.com/1.json'
 
     def get(self):
+        """Get data of databse on json format from the url
+         Returns
+         -------
+         list of str
+            all the information from the database
+            """
         result = requests.get(self.url)
         return json.loads(result.content.decode())
+        # Database to test the app
         # with open('1.json') as json_file:
         #     data = json.load(json_file)
         #     return data
 
-    # Update existing data or add new data
-    def patch(self, JSON, categoria):
+    def patch(self, JSON, category):
+        """Update existing data or add new data
+        Parameters
+        ----------
+        JSON: list of str
+        category: str"""
         to_database = json.loads(JSON)
-        requests.patch(url = self.url[:-5] + '/' + categoria + '.json', json = to_database)
+        requests.patch(url = self.url[:-5] + '/' + category + '.json', json = to_database)
 
-    # Add data with a unique key, not recomended
     def post(self, JSON):
+        """Add data with unique key, not recomended
+        Parameters
+        ----------
+        JSON: list of str"""
         to_database = json.loads(JSON)
         requests.post(url = self.url, json = to_database)
 
-    # Add data, but delete the rest of the data
     def put(self, JSON):
+        """Add data, but delete the rest of the data
+        Parameters
+        ----------
+        JSON: list of str"""
         to_database = json.loads(JSON)
         requests.put(url = self.url, json = to_database)
 
-    # Delete the specific child
     def delete(self, category, title):
-        requests.delete(url = 'https://recetas-acfc9.firebaseio.com/1/%s/%s.json' %(category, title))
-
-    
+        """Delete the specific child of the database
+        Parameters
+        ----------
+        category: str
+        title: str"""
+        requests.delete(url = 'https://recetas-acfc9.firebaseio.com/1/%s/%s.json' %(category, title))    
